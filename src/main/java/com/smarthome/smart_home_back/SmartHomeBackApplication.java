3ddfea860d6e7404f25e4dfd9f2e11586935319a
jose manuel <jsipacc@miumg.edu.gt>
2026-09-13 21:10:24 -0600

    Initial commit - Spring Boot Smart Home

---
+++ src/main/java/com/smarthome/smart_home_back/SmartHomeBackApplication.java
@@ -0,0 +1,13 @@
+package com.smarthome.smart_home_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smarthome/smart_home_back/SmartHomeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthome.smart_home_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
